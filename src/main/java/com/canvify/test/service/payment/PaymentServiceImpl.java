0b96fe39ce29cd486e16f2df558105f1654a4a01
ashishgupta7206 <104692812+ashishgupta7206@users.noreply.github.com>
2026-01-17 15:43:49 +0530

    updated payments

--- src/main/java/com/canvify/test/service/payment/PaymentServiceImpl.java
+++ src/main/java/com/canvify/test/service/payment/PaymentServiceImpl.java
@@ -93,7 +93,6 @@ public class PaymentServiceImpl implements PaymentService {
     // RAZORPAY WEBHOOK
     // -------------------------------------------------
     @Override
-    @Transactional
     public ApiResponse<?> handleProviderWebhook(
             ProviderWebhookDTO webhook,
             String signatureHeader,
